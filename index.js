0802b793cefb00680690281e0a7629eee40b4cd4
brenda <lamasbrenda@hotmail.com>
2022-01-16 21:23:46 -0300

    fetch ubicacion

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ const inputBusqueda = document.querySelector("#input-busqueda")
 const sectionTarjetas = document.querySelector("#section-tarjetas")
 const sectionBusqueda = document.querySelector("#section-busqueda")
 const sectionDetalle = document.querySelector("#section-detalle")
+const selectUbicacion = document.querySelector("#select-ubicacion")
 
 
-const buscarProductos = (producto) =>{
-    fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${producto}`)
+//FETCH
+const buscarProductos = (producto,direccion) =>{
+    fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${producto}&state=${direccion}`)
     .then(res => res.json())
     .then(data =>{
-        console.log(data.results);
-        mostrarTarjetas(data.results)
-        filtrarCondicion(data.results)
+        console.log(data);
+        mostrarTarjetas(data.results, data.available_filters.state)
+        console.log(data.available_filters.state);
     })
 }
+
 const verProducto = (id)=>{
     fetch(`https://api.mercadolibre.com/items/${id}`)
     .then(res => res.json())
@@ -24,25 +27,30 @@ const verProducto = (id)=>{
 
 }
 
-const mostrarTarjetas = (producto) =>{
+form.onsubmit=(e)=>{
+    e.preventDefault()
+    buscarProductos(inputBusqueda.value, selectUbicacion.value)
+}
+
+const mostrarTarjetas = (producto, direccion) =>{
     sectionTarjetas.style.display = "flex"
     sectionTarjetas.innerHTML= producto.reduce((acc, curr)=>{
-        return acc + ` 
+        return acc + `
+        
         <div class="tarjetas" data-id="${curr.id}">
             <img class="img-tarjeta" src="${curr.thumbnail}">
             <div class="text-tarjetas">
                 <h2>${curr.title}</h2>
                 <p>$${curr.price}</p>
+                <p>${curr.address.state_name}</p>
+
             </div>
             
         </div>`
-    },"")
+    },`<button id="prev">Pagina Anterior</button>
+    <button id="next">Pagina Siguiente</button>`)
     clickATarjetas()
-}
 
-form.onsubmit=(e)=>{
-  e.preventDefault()
-  buscarProductos(inputBusqueda.value)
 }
 
 const clickATarjetas = () =>{
@@ -54,6 +62,8 @@ const clickATarjetas = () =>{
         }  
     }
 }
+
+
 ////// TARJETAS EN DETALLE
 const detalleTarjeta = (data)=>{
     sectionTarjetas.style.display = "none"
@@ -64,10 +74,10 @@ const detalleTarjeta = (data)=>{
         <h2>${data.title}</h2>
         <img src="${data.thumbnail}">
         <p>$${data.base_price}</p>
+        <p>${mostrarEnvioGratis(data.shipping.free_shipping)}</p>
     </article>
-    <button class="boton-atras" id="${data.id}">Atrás</button>
+    <button class="boton-atras" id="${data.id}">Atrás</button>`
 
-    `
     const botonAtras = document.querySelector(".boton-atras")
 
     botonAtras.onclick = () => {
@@ -77,20 +87,19 @@ const detalleTarjeta = (data)=>{
     }
 
 }
-
-// FILTRAR POR CONDICION
-
-// si la condicion es nuevo --> mostrame estos
-// si la condicion es no es nuevo --> mostrame el resto
-const selectOrden = document.querySelector("#select-orden")
-selectOrden.onchange=()=>{
-    buscarProductos()
-}
-const filtrarCondicion = (data) =>{
-    if (data.condition === "new") {
-        console.log("es nuevo");
+const mostrarEnvioGratis = (tipoEnvio)=>{
+    if (tipoEnvio === true) {
+        return "Envio Gratuito"
+    }else{
+        return "Envio normal"
     }
 }
 
 
-  
+// const next = document.querySelector("#next")
+// let paginaActual = 1
+
+// next.onclick=()=>{
+//     paginaActual ++
+//     buscarProductos()
+// }
